efbcf59fba41171ce9a310bee2a5b45295fe0c44
Falco Hirschenberger <falco.hirschenberger@gmail.com>
2026-09-09 13:42:28 +0200

    Add itoa function and log delayed frames

--- src/macros.h
+++ src/macros.h
@@ -8,6 +8,15 @@
     call uart_write_byte
 .endm
 
+.macro LOG_STRING_PTR reg
+    mv a0, \reg
+    call uart_write_string
+
+    ## append a newline to flush
+    li a0, '\n'
+    call uart_write_byte
+.endm
+
 .macro LOAD_WORD reg addr
     la \reg, \addr
     lw \reg, 0(\reg)
